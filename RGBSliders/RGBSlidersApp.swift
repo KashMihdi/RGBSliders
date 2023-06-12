57b707561af065cc863fddae6af92107cc9ed2e6
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-06-12 13:49:57 +0300

    Initial Commit

---
+++ RGBSliders/RGBSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSlidersApp.swift
+//  RGBSliders
+//
+//  Created by Kasharin Mikhail on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
